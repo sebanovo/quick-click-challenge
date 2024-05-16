6b168233a32633e1aa650605d2dc84587bac3d22
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2024-05-16 09:22:44 -0400

    refacto all

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Boton from './Components/Boton'
 import Contador from './Components/Contador'
 export default function App() {
   const [count, setCount] = useState(0)
   const [clock, setClock] = useState(0)
-  const [puntaje, setPuntaje] = useState(0)
+  const [puntaje, setPuntaje] = useState(() => {
+    return Number(window.localStorage.getItem('score'))
+  })
+  const refReloj = useRef(null)
 
   useEffect(() => {
     const interval = setInterval(() => {
       setClock(prevClock => {
-        if(prevClock === 10){
+        if (prevClock === 10) {
           setClock(1)
           setPuntaje(prevPuntaje => {
-            let $puntaje = document.getElementsByClassName("contador")[0].innerHTML
-            if(prevPuntaje < parseInt($puntaje)){
-              return $puntaje
+            let score = refReloj.current.innerHTML
+            if (prevPuntaje < parseInt(score)) {
+              window.localStorage.setItem('score', score)
+              return score
             }
             return prevPuntaje
           })
@@ -42,17 +46,16 @@ export default function App() {
 
   return (
     <>
-      <div className="reloj">{clock}</div>
       <main>
-        <h1 className="titulo">Botón</h1>
+        <h1 className='titulo'>
+          Quick Click
+          <div className='reloj' ref={refReloj}>
+            {clock}
+          </div>
+        </h1>
         <h2>Mejor puntuación es: {puntaje}</h2>
-        <Contador numberClicks={count}></Contador>
-        <Boton
-          esBotonClick={true}
-          texto={'click'}
-          handleClik={subirContador}
-        ></Boton>
-        <Boton esBotonClick={false} texto="reset" handleClik={reset}></Boton>
+        <Contador numberClicks={count} subirContador={subirContador}></Contador>
+        <Boton esBotonClick={false} texto='reset' handleClik={reset}></Boton>
       </main>
     </>
   )
--- src/Components/Contador.jsx
+++ src/Components/Contador.jsx
@@ -1,8 +1,8 @@
 import '../styles/Contador.css'
-export default function Contador({ numberClicks }) {
+export default function Contador({ numberClicks, subirContador }) {
   return (
-    <div className="contador-container">
-      <h1 className="contador">{numberClicks}</h1>
+    <div className='contador-container' onClick={subirContador}>
+      <h1 className='contador'>{numberClicks}</h1>
     </div>
   )
 }
